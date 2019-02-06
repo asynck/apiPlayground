193a65884cf51ac6339250878a211053f9a53545
fragkog <kfragkog@gmail.com>
2019-02-06 14:41:40 +0000

    updates

--- app.js
+++ app.js
@@ -1,13 +1,34 @@
+//DOM
+let main = document.getElementById("main");
+
+//fetch data
 fetch("https://ghibliapi.herokuapp.com/films")
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(myJson) {
-        console.log(myJson);
-        myJson.forEach( function(film) {
-            console.log(film.title);
-        });;
-    })
-    .catch(function(error) {
-        console.error("Error: ", error);
+  .then(function(response) {
+    if (!response.ok) {
+      console.error("HTTP error, status = " + response.status);
+    }
+    return response.json();
+  })
+  .then(function(myJson) {
+    console.log(myJson);
+    myJson.forEach(function(film) {
+      let movie = document.createElement("div");
+      movie.classList.add("movie", "border");
+      main.appendChild(movie);
+      let div1 = document.createElement("div");
+      let div2 = document.createElement("div");
+      let div3 = document.createElement("div");
+      div1.innerText = film.title;
+      div2.innerText = film.director + ", " + film.release_date;
+      div3.innerText = film.description.substring(0, 400) + "...";
+      div1.classList.add("title");
+      div2.classList.add("director");
+      div3.classList.add("description");
+      movie.appendChild(div1);
+      movie.appendChild(div2);
+      movie.appendChild(div3);
     });
+  })
+  .catch(function(error) {
+    console.error("Error: ", error);
+  });
